76a9d7dac1640b01b9c42fe9affe70638ee27714
jens0486 <jens0486@web.de>
2011-08-10 17:40:51 +0200

    wichtig

--- gen/de/seideman/dams/activities/R.java
+++ gen/de/seideman/dams/activities/R.java
@@ -11,7 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int interfaceListView=0x7f050000;
+        public static final int interfaceListView=0x7f050002;
+        public static final int myBlack=0x7f050001;
+        public static final int myGrey=0x7f050000;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
@@ -20,13 +22,13 @@ public final class R {
         public static final int ExpandableListView01=0x7f060002;
         public static final int TableRow01=0x7f060001;
         public static final int TableRow02=0x7f060003;
-        public static final int TableRow03=0x7f06002a;
-        public static final int TableRow04=0x7f060027;
+        public static final int TableRow03=0x7f06002b;
+        public static final int TableRow04=0x7f060028;
         public static final int TextView01=0x7f060004;
         public static final int TextView02=0x7f06000b;
         public static final int btnLogin=0x7f06000e;
         public static final int btnSearch=0x7f060017;
-        public static final int listView1=0x7f06002e;
+        public static final int listView1=0x7f060018;
         public static final int listView2=0x7f06000a;
         public static final int spinner1=0x7f060010;
         public static final int spinner2=0x7f060012;
@@ -38,32 +40,32 @@ public final class R {
         public static final int tableRow5=0x7f06000f;
         public static final int tableRow6=0x7f060011;
         public static final int tableRow7=0x7f060013;
-        public static final int textHeight=0x7f060026;
-        public static final int textHostname=0x7f060018;
-        public static final int textIP=0x7f06001b;
-        public static final int textInventar=0x7f06001a;
-        public static final int textLocation=0x7f060021;
-        public static final int textManufactor=0x7f060023;
-        public static final int textModell=0x7f060025;
+        public static final int textHeight=0x7f060027;
+        public static final int textHostname=0x7f060019;
+        public static final int textIP=0x7f06001c;
+        public static final int textInventar=0x7f06001b;
+        public static final int textLocation=0x7f060022;
+        public static final int textManufactor=0x7f060024;
+        public static final int textModell=0x7f060026;
         public static final int textPass=0x7f06000d;
-        public static final int textRack=0x7f06001f;
+        public static final int textRack=0x7f060020;
         public static final int textSearchValue=0x7f060015;
-        public static final int textSerial=0x7f06001d;
-        public static final int textStatus=0x7f06002c;
-        public static final int textTyp=0x7f060029;
+        public static final int textSerial=0x7f06001e;
+        public static final int textStatus=0x7f06002d;
+        public static final int textTyp=0x7f06002a;
         public static final int textUser=0x7f06000c;
         public static final int textView1=0x7f060008;
-        public static final int textView11=0x7f060022;
-        public static final int textView13=0x7f060024;
-        public static final int textView15=0x7f060028;
-        public static final int textView17=0x7f06002b;
-        public static final int textView19=0x7f06002d;
+        public static final int textView11=0x7f060023;
+        public static final int textView13=0x7f060025;
+        public static final int textView15=0x7f060029;
+        public static final int textView17=0x7f06002c;
+        public static final int textView19=0x7f06002e;
         public static final int textView2=0x7f060006;
-        public static final int textView23=0x7f060019;
+        public static final int textView23=0x7f06001a;
         public static final int textView3=0x7f060014;
-        public static final int textView5=0x7f06001c;
-        public static final int textView7=0x7f06001e;
-        public static final int textView9=0x7f060020;
+        public static final int textView5=0x7f06001d;
+        public static final int textView7=0x7f06001f;
+        public static final int textView9=0x7f060021;
     }
     public static final class layout {
         public static final int connection_info_dialog=0x7f030000;
